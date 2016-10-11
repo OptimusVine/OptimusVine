0993d369a1eb426f91aee724071211822d4fbab6
KJ <kjiel.carlson@gmail.com>
2016-10-11 15:33:09 -0700

    Fix Port

--- app.js
+++ app.js
@@ -45,14 +45,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-var server = app.listen(8081, function () {
+app.set('port', (process.env.PORT || 5000));
 
-  var host = '127.0.0.1'
-  var port = server.address().port
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
-  console.log("Example app listening at http://%s:%s", host, port)
+ // console.log("Example app listening at http://%s:%s", host, port)
 
-})
 
 module.exports = app;
 
