0a1dab6d57f02b32ac2bd3e9a42beed7030a0a5e
KJ <kjiel.carlson@gmail.com>
2016-10-11 15:18:32 -0700

    DB Setup

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 module.exports = {
 	url: {
 		development: 'mongodb://localhost/fromScratch',
-	//	production: 'mongodb://admin:Target#33@ds055535.mongolab.com:55535/pvc'
+		production: 'mongodb://admin:Target#33@ds055535.mongolab.com:55535/pvc'
 	}
 
 }
